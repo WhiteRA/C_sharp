e4fb91d350a194e2c20671997eaa4a10d60e56da
Parhomenko Aleksandr <WhiteRA_@mail.ru>
2022-12-01 21:23:09 +1000

    правки файла

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Первое ДЗ
+//Задача 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Начало");
+int namber1 = 2;
+int namber2 = 3;
+int namber3 = 7;
+Console.WriteLine("Конец");
