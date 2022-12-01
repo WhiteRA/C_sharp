5d510fef0f524f68becfcde36fe235aa0efdb41f
Parhomenko Aleksandr <WhiteRA_@mail.ru>
2022-12-02 02:01:30 +1000

    Выполненно третье задиние

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //Первое ДЗ
 //Задача 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+Console.WriteLine("Задание 1");
 Console.WriteLine("Начало");
 int a = 2;
 int b = 3;
@@ -13,10 +14,12 @@ if (c > max) max = c;
 Console.Write("max = ");
 Console.WriteLine(max);
 Console.WriteLine("Конец");
+Console.WriteLine(" ");
 
 
 //Задача 2: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+Console.WriteLine("Задание 2");
 Console.WriteLine("Начало");
 int w = 4;
 int s = -3;
@@ -28,3 +31,55 @@ if (x % 2 == 0) even = x;
 Console.Write("Чётное число = ");
 Console.WriteLine(even);
 Console.WriteLine("Конец");
+Console.WriteLine(" ");
+
+//Задача 3: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Задание 3");
+Console.WriteLine("Начало");
+int e = 5;
+int f = 8;
+int i = 0;
+int j = 0;
+int even1 = 0;
+int even2 = 0;
+while (i < e)
+{
+    if (i % 2 == 0)
+    {
+        if (i == 0)
+        {
+
+        }
+        else
+        {
+            even1 = i;
+            Console.Write("Чётное число = ");
+            Console.WriteLine(even1);
+        }
+    }
+    
+    i++;
+}
+Console.WriteLine("-----");
+
+while (j < f)
+{
+    if (j % 2 == 0)
+    {
+        if (j == 0)
+        {
+
+        }
+        else
+        {
+            even2 = j;
+            Console.Write("Чётное число = ");
+            Console.WriteLine(even2);
+        }
+    }
+    j++;
+}
+Console.WriteLine("-----");
+Console.WriteLine("Конец");
+Console.WriteLine(" ");
