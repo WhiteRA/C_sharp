ba8eb12f273341731c4e84384064f7a6fc69267b
Parhomenko Aleksandr <WhiteRA_@mail.ru>
2022-12-09 01:08:41 +1000

    добавил новые методы в файл библиотеки и доделал 3е дз

--- Library.cs
+++ Library.cs
@@ -6,7 +6,7 @@
     public static class Library
     {
         /// <summary>
-        /// Этот метод генерирует случайное число от 10 до 999
+        /// Метод генерирует случайное число от 10 до 999
         /// </summary>
         /// <returns></returns>
         public static int RandomNumbers_3()
@@ -16,13 +16,38 @@
         }
 
         /// <summary>
-        /// Этот метод генерирует случайное число от 1 до 7 
+        /// Метод генерирует случайное число от 1 до 10 
         /// </summary>
         /// <returns></returns>
         public static int RandomNumbers_1()
         {
-            int RandomNumbers = new Random().Next(1, 7);
+            int RandomNumbers = new Random().Next(1, 10);
             return RandomNumbers;
+        }
+
+        /// <summary>
+        /// Метод создиня массива
+        /// </summary>
+        /// <param name="count"> Является колличеством элементов массива</param>
+        /// <returns></returns>
+        public static int[] CreateArr(int count)
+        {
+            return new int[count];
+        }
+
+        /// <summary>
+        /// Метод заполнения массива
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="min">Нижний предел генерации рандома размера массива</param>
+        /// <param name="max">Верхний предел генерации рандома размера массива</param>
+        public static void Fill(int[] array, int min, int max)
+        {
+            int size = array.Length;
+            for(int i = 0; i < size; i++)
+            {
+                array[i] = new Random().Next(min, max);
+            }
         }
     }
 }
