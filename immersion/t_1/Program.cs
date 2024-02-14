44c7dfabfa82b66232fa20bc1c03f50fb095666d
mr.White <whitera_@mail.ru>
2024-02-14 19:59:54 +1000

    +

--- immersion/t_1/Program.cs
+++ immersion/t_1/Program.cs
@@ -10,6 +10,39 @@ namespace t_1
     {
         static void Main(string[] args)
         {
+            if (args.Length == 3)
+        {
+            char sign = args[1][0];
+            if (int.TryParse(args[0], out int num1) && int.TryParse(args[2], out int num2))
+            {
+                switch (sign)
+                {
+                    case '+':
+                        System.Console.WriteLine(num1 + num2);
+                        break;
+                    case '-':
+                        System.Console.WriteLine(num1 - num2);
+                        break;
+                    case '*':
+                        System.Console.WriteLine(num1 * num2);
+                        break;
+                    case '/':
+                        System.Console.WriteLine(num1 / num2);
+                        break;
+                    default:
+                        System.Console.WriteLine("Такого символа нет");
+                        break;
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Правильный ввод а + в ");
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("Неверный ввод");
+        }
         }
     }
 }
