cc30282947fcafb6f02a571176fca1448621ecd8
mr.White <whitera_@mail.ru>
2024-03-21 21:14:38 +1000

    +

---
+++ dev/app/L_6/t_1/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t_1
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Деление на ноль");
+            }
+            return (double)a / b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Деление на ноль");
+            }
+            return a / b;
+        }
+    }
+
+}
---
+++ dev/app/L_6/t_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            // Целые числа
+            Console.WriteLine("Сложение (целые числа): " + calc.Add(5, 3));
+            Console.WriteLine("Вычитание (целые числа): " + calc.Subtract(5, 3));
+            Console.WriteLine("Умножение (целые числа): " + calc.Multiply(5, 3));
+            Console.WriteLine("Деление (целые числа): " + calc.Divide(5, 3));
+
+            // Дробные числа
+            Console.WriteLine("Сложение (дробные числа): " + calc.Add(5.5, 3.3));
+            Console.WriteLine("Вычитание (дробные числа): " + calc.Subtract(5.5, 3.3));
+            Console.WriteLine("Умножение (дробные числа): " + calc.Multiply(5.5, 3.3));
+            Console.WriteLine("Деление (дробные числа): " + calc.Divide(5.5, 3.3));
+        }
+    }
+
+}
