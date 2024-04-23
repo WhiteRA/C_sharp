26083e15f17c039f34db2efe5e97c4ee370e7769
Aleksandr Parhomenko <whitera_@mail.ru>
2024-04-23 11:03:23 +1000

    +

---
+++ dev/net/Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace L_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint removeEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            Console.WriteLine("кудато цепляюсь");
+            client.Connect(removeEndPoint);
+            Console.WriteLine("подцепился");
+            client.Close();
+        }
+    }
+}
