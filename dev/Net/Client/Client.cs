0bc116477065251bd37cba82b216f23823c5df5e
mr.White <whitera_@mail.ru>
2024-05-23 23:11:25 +1000

    Чат через UDP странный мув... Чего не сделаешь ради учёбы

---
+++ dev/Net/Client/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public static class Client
+    {
+        static void Main(string[] args)
+        {
+            string userName = args.Length > 0 ? args[0] : "DefaultUser";
+            string serverIP = args.Length > 1 ? args[1] : "127.0.0.1";
+            int serverPort = 11000;
+
+            UdpClient client = new UdpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Ввод сообщения: ");
+                    string message = Console.ReadLine();
+                    string fullMessage = $"{userName}: {message}";
+                    byte[] sendBytes = Encoding.UTF8.GetBytes(fullMessage);
+
+                    client.Send(sendBytes, sendBytes.Length, serverEndPoint);
+
+                    client.Client.ReceiveTimeout = 5000; // Таймаут ожидания 5 секунд
+                    try
+                    {
+                        byte[] receiveBytes = client.Receive(ref serverEndPoint);
+                        string response = Encoding.UTF8.GetString(receiveBytes);
+                        Console.WriteLine("Ответ сервера: " + response);
+                    }
+                    catch (SocketException)
+                    {
+                        Console.WriteLine("Подтверждение от сервера не получено.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ dev/Net/Server/Server.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Server
+    {
+        static void Main(string[] args)
+        {
+            int port = 11000;
+            UdpClient server = new UdpClient(port);
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+            try
+            {
+                Console.WriteLine("Ожидает подключения...");
+
+                while (true)
+                {
+                    byte[] receiveBytes = server.Receive(ref clientEndPoint);
+                    string receivedData = Encoding.UTF8.GetString(receiveBytes);
+                    Console.WriteLine("Получено: " + receivedData);
+
+                    string confirmationMessage = "Сообщение получено.";
+                    byte[] confirmationBytes = Encoding.UTF8.GetBytes(confirmationMessage);
+                    server.Send(confirmationBytes, confirmationBytes.Length, clientEndPoint);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                server.Close();
+            }
+        }
+    }
+}
